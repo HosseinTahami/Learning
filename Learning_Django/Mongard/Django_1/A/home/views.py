e6134abb4a9a8c14b3ed0810dc243bebb9d27774
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-08 11:41:33 +0330

    testing

--- Learning_Django/Mongard/Django_1/A/home/views.py
+++ Learning_Django/Mongard/Django_1/A/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
-def say_hello():
-    return HttpRequest(" Hello MFs!")
+def say_hello(request):
+    return HttpResponse(" Hello MFs!")
