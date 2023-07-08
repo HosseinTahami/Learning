d00c38cc8a64dca884b8399292238cfb4c58530e
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-08 16:51:13 +0330

    tem -->  template

--- Learning_Django/Mongard/Django_1/A/A/settings.py
+++ Learning_Django/Mongard/Django_1/A/A/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'A.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'tem'],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
