2c33309a1bd3201a4125c46502b6698788574655
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-08 12:25:26 +0330

    home page, bye page, hello page

--- Learning_Django/Mongard/Django_1/A/home/urls.py
+++ Learning_Django/Mongard/Django_1/A/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     
+    path('', views.home),
     path('hello/', views.say_hello),
     path('bye/', views.say_bye),
     
--- Learning_Django/Mongard/Django_1/A/home/views.py
+++ Learning_Django/Mongard/Django_1/A/home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
 def say_hello(request):
     return HttpResponse(" Hello MFs!")
 
