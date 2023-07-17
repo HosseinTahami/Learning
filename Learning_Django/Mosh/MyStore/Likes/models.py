87faabe97ef2aba3e91c0749393c8a793830b156
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-17 11:59:42 +0330

    LikedItem model created

--- Learning_Django/Mosh/MyStore/Likes/models.py
+++ Learning_Django/Mosh/MyStore/Likes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+class LikedItem(models.Model):
+    # What user liked  what object
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    # Type (product, video, article, ...) & ID
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE
+        )
+    object_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey()
