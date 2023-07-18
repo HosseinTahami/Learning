9fc0336414691a4e80f60dc4e9039891ca15ff19
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-18 16:16:07 +0330

    nested class in forms.py

--- Learning_Django/Mongard/Django_1/A/home/forms.py
+++ Learning_Django/Mongard/Django_1/A/home/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-
+from .models import Todo
 class TodoCreateForm(forms.Form):
     title = forms.CharField()
     body = forms.CharField(label='Description', required=False)
     created = forms.DateTimeField()
+
+class TodoUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ('title', 'body', 'created')
