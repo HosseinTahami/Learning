8d29132da63b7e3d159540d1b8535fbe109a59c3
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 09:16:53 +0330

    views.py user_register updated

--- Learning_Django/Mongard/Django_1/A/accoutns/views.py
+++ Learning_Django/Mongard/Django_1/A/accoutns/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
 def user_register(request):
-    return HttpResponse('User registration page')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            pass
+    else:
+        form = UserRegistrationForm()
+        return render(request, 'register.html', {'form':form})
+        
