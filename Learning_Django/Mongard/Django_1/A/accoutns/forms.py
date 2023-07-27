06a93b7bb3fda237316c395cb54016419db05fd7
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 09:14:03 +0330

    forms.py for accounts created

---
+++ Learning_Django/Mongard/Django_1/A/accoutns/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(
+        widget=forms.PasswordInput
+        )
