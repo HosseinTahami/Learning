8db2139d04c65c392cb6954d40332f1735f169b8
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 21:55:22 +0330

    cleaner code

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
@@ -5,14 +5,15 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 class UserRegister(View):
+    form_class = UserRegisterForm
     def get(self, request):
-        form = UserRegisterForm()
+        form = self.form_class()
         return render(request,
                       'accounts/register.html',
                       {'form': form}
                     )
     def post(self, request):
-        form = UserRegisterForm(request.POST)
+        form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             User.objects.create_user(
