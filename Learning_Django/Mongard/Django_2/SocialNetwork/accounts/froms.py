e9efa7dc07f70e62a42cad7b06294125082a8661
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 15:27:48 +0330

    UserRegisterForm

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/froms.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/froms.py
@@ -1,2 +1,6 @@
 from django import forms
 
+class UserRegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
