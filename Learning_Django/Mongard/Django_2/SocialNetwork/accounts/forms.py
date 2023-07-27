2f648836c07bb4ea848959f3649c8a857009158a
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 16:10:55 +0330

    Playing with widgets

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/forms.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/forms.py
@@ -1,6 +1,28 @@
 from django import forms
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Username'
+            }
+        )
+    )
+    
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Example@email.com'
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }
+        )
+    )
