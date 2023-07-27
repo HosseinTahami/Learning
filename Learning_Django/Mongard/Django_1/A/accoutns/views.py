d241eb7ce8c1a9665c01cb6ac30f0a5623604b86
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 10:36:41 +0330

    create user_login function

--- Learning_Django/Mongard/Django_1/A/accoutns/views.py
+++ Learning_Django/Mongard/Django_1/A/accoutns/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -22,3 +22,11 @@ def user_register(request):
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form':form})
 
+
+def user_login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form':form})
+    
