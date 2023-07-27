ee73f10b4199d7eeb14e4e274ad401ee5f476dbf
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 21:34:54 +0330

    user is now getting created

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 class UserRegister(View):
     def get(self, request):
@@ -10,5 +12,17 @@ class UserRegister(View):
                       {'form': form}
                     )
     def post(self, request):
-        pass
-
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(
+                cd['username'],
+                cd['email'],
+                cd['password']
+            )
+            messages.success(
+                request,
+                'Account created successfully',
+                'success'
+                )
+            return redirect('home:home')
