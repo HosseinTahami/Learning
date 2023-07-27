3848a84b88edab7022f9f17a0e0f77444bd7ee4c
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 09:59:27 +0330

    debug

--- Learning_Django/Mongard/Django_1/A/accoutns/views.py
+++ Learning_Django/Mongard/Django_1/A/accoutns/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 def user_register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            pass
+            cd = form.cleaned_data
+            User.objects.create_user(
+                cd['username'],
+                cd['email'],
+                cd['password']
+            )
+            return redirect('home')
     else:
         form = UserRegistrationForm()
-        return render(request, 'register.html', {'form':form})
-        
+    return render(request, 'register.html', {'form':form})
+
