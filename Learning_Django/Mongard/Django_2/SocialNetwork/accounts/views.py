d76dd6f05b30ab1779698372c07dea3f8b1d9b34
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 15:19:59 +0330

    views.py of accounts updated

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 
 class UserRegister(View):
     def get(self, request):
-        pass
+        return render(request, 'accounts/register.html')
     def get(self, request):
         pass
 
