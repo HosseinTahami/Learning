c9f7e803457b9744f733e25a2e9755141d83311c
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 15:33:38 +0330

    typo in file name

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/froms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class UserRegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
