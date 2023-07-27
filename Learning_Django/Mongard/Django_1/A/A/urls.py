d58feba35c887359c02bc7d888ef9a71bcd49527
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 09:03:58 +0330

    accounts in urls of project added

--- Learning_Django/Mongard/Django_1/A/A/urls.py
+++ Learning_Django/Mongard/Django_1/A/A/urls.py
@@ -21,5 +21,6 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('__debug__/', include(debug_toolbar.urls))
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('accounts/', include('accounts.urls')),
 ]
