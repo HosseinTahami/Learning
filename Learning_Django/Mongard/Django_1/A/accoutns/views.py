a897dd450340b2bc1ea620aa05da865c07a2dfb8
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 09:02:21 +0330

    user_registration created

--- Learning_Django/Mongard/Django_1/A/accoutns/views.py
+++ Learning_Django/Mongard/Django_1/A/accoutns/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def user_register(request):
+    return HttpResponse('User registration page')
