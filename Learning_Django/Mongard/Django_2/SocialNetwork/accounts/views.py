a23043a576e2a581b04e986ffca80a0a014471fd
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 15:33:26 +0330

    views.py updated

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views import View
-
+from .forms import UserRegisterForm
 
 class UserRegister(View):
     def get(self, request):
-        return render(request, 'accounts/register.html')
+        form = UserRegisterForm()
+        return render(request,
+                      'accounts/register.html',
+                      {'form': form}
+                    )
     def post(self, request):
         pass
 
