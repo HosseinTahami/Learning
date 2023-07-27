6a5ecf426f7f30b36b8099b736a803923dc2a1ff
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 15:24:26 +0330

    typo

--- Learning_Django/Mongard/Django_2/SocialNetwork/SocialNetwork/settings.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/SocialNetwork/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'account.apps.AccountsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
