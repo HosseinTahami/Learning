5d4c2065d726851548d7b1d275198214f0764bf3
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-27 15:25:12 +0330

    two get functions the second one become post

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
@@ -5,6 +5,6 @@ from django.views import View
 class UserRegister(View):
     def get(self, request):
         return render(request, 'accounts/register.html')
-    def get(self, request):
+    def post(self, request):
         pass
 
