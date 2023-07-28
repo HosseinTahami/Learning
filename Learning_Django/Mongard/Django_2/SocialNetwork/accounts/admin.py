51298986f4a37f13ae44f7d3ec37ac7805fd000a
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-28 17:06:21 +0330

    Extend User

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/admin.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+
+class ExtendedUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)
