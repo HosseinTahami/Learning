1ee09d55343e3af954e0a3be217365a527605b3a
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-28 09:08:18 +0330

    forms for UserLoginForm

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/forms.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/forms.py
@@ -56,3 +56,25 @@ class UserRegisterForm(forms.Form):
         confirm_password = cleaned_data.get('confirm_password')
         if password and confirm_password and password != confirm_password:
             raise ValidationError('Password does not match.')
+
+
+class UserLoginForm(forms.Form):
+    
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Username'
+            }
+        )
+        
+    )
+    
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }
+        )
+    )
