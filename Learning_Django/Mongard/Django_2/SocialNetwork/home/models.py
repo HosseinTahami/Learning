db6f3183391882049203f8c2ca9b806965291dcf
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-28 15:36:18 +0330

    .

--- Learning_Django/Mongard/Django_2/SocialNetwork/home/models.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    slug = models.SlugField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
