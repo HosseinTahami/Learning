cbc7a7f116563f9190dbc50b8d98802d653ad16b
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-28 16:50:05 +0330

    create profile

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/models.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+        )
+    
+    age = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True
+        )
+    bio = models.TextField(
+        null=True,
+        blank=True
+        )
