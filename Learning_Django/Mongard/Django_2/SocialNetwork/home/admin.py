085b42947a5f5d52449cb0065d1a2f0b9c374fb0
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-28 16:18:48 +0330

    customize admin panel

--- Learning_Django/Mongard/Django_2/SocialNetwork/home/admin.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/home/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'body',)
+    search_fields = ('user', 'body',)
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
