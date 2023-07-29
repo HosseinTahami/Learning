eebe2c0b3ec14f51e91e62fa0c093a8ec0c7429d
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-29 08:42:15 +0330

    .

--- Learning_Django/Mongard/Django_3/OnlineStore/accounts/models.py
+++ Learning_Django/Mongard/Django_3/OnlineStore/accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+
+class User(AbstractBaseUser):
+    full_name = models.CharField(max_length=225)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(
+        max_length=11,
+        unique=True
+        )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    # This Field is called username but actually it is the field we authenticate our users with it !
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+    
+    def __str__(self) -> str:
+        return f'Full Name: {self.full_name}, Phone Number: {self.phone_number}, Email: {self.email}'
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
