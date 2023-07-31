f468de5967ac2455a9a86995066cc933e92c4e8c
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-31 17:53:31 +0330

    post in views.py

--- Learning_Django/Mongard/Django_3/OnlineStore/accounts/views.py
+++ Learning_Django/Mongard/Django_3/OnlineStore/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import UserRegistrationForm
-
+from utils import send_top_code
+import random
 
 class UserRegisterView(View):
     form_class = UserRegistrationForm
@@ -12,4 +13,8 @@ class UserRegisterView(View):
         return render(request, self.template, {"form": form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            random_code = random.randint(1000, 9999)
+            send_top_code(cd['phone_number'], random_code)
