f2c9dbc1a408c7cf575ac28a1607e63080eda739
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-06 09:54:42 +0330

    Car Object added

--- Learning_JavaScript/first.js
+++ Learning_JavaScript/first.js
@@ -6,4 +6,16 @@ function Person(name, age, gender){
 }
 
 var P1 = new Person("Hossein", "22", "Male");
-console.log(P1.age)
+console.log(P1.age);
+
+function Car(name, price){
+    this.name = name;
+    this.price = price;
+    this.final_price = function(discount){
+        this.price = price * (100-discount)/100;
+        console.log(this.price)
+    }
+}
+
+var C1 = new Car("BMW", 100000);
+C1.final_price(10);
