304745e31aa3407d1e5dadcfb80b0814202b7602
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 15:12:04 +0330

    add about

--- Learning_Django/Mongard/Django_5/Home/urls.py
+++ Learning_Django/Mongard/Django_5/Home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, About
 
 app_name = "Home"
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("about/", About.as_view(), name="about"),
 ]
