24479b1296528151a3b12061b32e8fa63ef9ab8b
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 15:20:10 +0330

    About with get_context_data

--- Learning_Django/Mongard/Django_5/Home/views.py
+++ Learning_Django/Mongard/Django_5/Home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+from .models import Car
 
 
 class Home(View):
@@ -46,3 +47,8 @@ class Home(View):
 
 class About(TemplateView):
     template_name = "Home/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cars"] = Car.objects.all()
+        return context
