586f5aa6587948c2f413afe7f81d845da163ba55
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 13:38:51 +0330

    add render

--- Learning_Django/Mongard/Django_5/Home/views.py
+++ Learning_Django/Mongard/Django_5/Home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
+
 class Home(View):
     def get(self, request):
-        pass
+        return render(request, "home/home.html")
+
     def post(self, request):
         pass
