48ec00b5c7c50d55bece3c2c5c5a409b3c89d778
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 18:16:27 +0330

    different url for CarDetailView added

--- Learning_Django/Mongard/Django_5/Home/urls.py
+++ Learning_Django/Mongard/Django_5/Home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, About, Main, CarsView
+from .views import Home, About, Main, CarsView, CarDetailView
 
 app_name = "Home"
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path("about/", About.as_view(), name="about"),
     path("main/<int:id>/<str:name>/", Main.as_view(), name="main"),
     path("cars/", CarsView.as_view(), name="cars"),
+    path("cars_detail/<int:car_id>", CarDetailView.as_view(), name="cars_detail"),
+    # path("cars_detail/<slug:my_slug>", CarDetailView.as_view(), name="cars_detail"),
+    # path("cars_detail/<str:name>/<int:build_year>/<str:owner>/", CarDetailView.as_view(), name="cars_detail"),
 ]
