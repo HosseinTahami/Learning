e9525b46269dc3bf3856605c20e2abff2584c83d
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 15:44:45 +0330

    main url added

--- Learning_Django/Mongard/Django_5/Home/urls.py
+++ Learning_Django/Mongard/Django_5/Home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, About
+from .views import Home, About, Main
 
 app_name = "Home"
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("about/", About.as_view(), name="about"),
+    path("main/<int:id>/<str:name>/", Main.as_view(), name="main"),
 ]
