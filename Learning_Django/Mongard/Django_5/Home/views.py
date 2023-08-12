0c9a267518c0d2ff166b496c74b0a5775cad68f8
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 14:04:21 +0330

    Home view completed

--- Learning_Django/Mongard/Django_5/Home/views.py
+++ Learning_Django/Mongard/Django_5/Home/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
 
 
 class Home(View):
+    http_method_names = [
+        "get",
+        "post",
+        "option",
+    ]
+
+    def setup(self, *args, **kwargs):
+        self.users = User.objects.all()
+        super().setup(self, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == "GET":
+            return self.get(request)
+        elif request.method == "POST":
+            return self.post(request)
+        elif request.method == "OPTIONS":
+            return self.options(request)
+        else:
+            raise ("Method Not Allowed")
+
     def get(self, request):
-        return render(request, "Home/home.html")
+        # users = User.object.all()
+        return render(request, "Home/home.html", {"users": self.users})
 
     def post(self, request):
-        pass
+        # users = User.object.all()
+        return render(request, "Home/home.html", {"users": self.users})
