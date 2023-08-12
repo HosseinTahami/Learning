01dc09acee5c077d26afac5cda026de087d884a5
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-12 14:16:24 +0330

    Home View completed

--- Learning_Django/Mongard/Django_5/Home/views.py
+++ Learning_Django/Mongard/Django_5/Home/views.py
@@ -31,3 +31,13 @@ class Home(View):
     def post(self, request):
         # users = User.object.all()
         return render(request, "Home/home.html", {"users": self.users})
+
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers["host"] = "localhost"
+        response.headers["user"] = request.user
+        return response
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        super().http_method_not_allowed(request, *args, **kwargs)
+        return render(request, "method_not_allowed.html")
