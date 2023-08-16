13e723c91d9ebc62f0c9a770ab187feff5027394
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-16 12:28:21 +0330

    Home & About added

--- Learning_Django/Mongard/Django_6/TestingInDjango/A/views.py
+++ Learning_Django/Mongard/Django_6/TestingInDjango/A/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return render(request, "home.html")
+
+
+class About(View):
+    def get(self, request, username):
+        return render(request, "home.html")
