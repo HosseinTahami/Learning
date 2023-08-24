19496dafac3f41beb4744998338b013ea21a0c09
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-24 19:23:36 +0330

    refactor: changed message from warning to danger

--- Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/accounts/views.py
@@ -7,106 +7,72 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 class UserRegister(View):
     form_class = UserRegisterForm
-    template_name = 'accounts/register.html'
-    
+    template_name = "accounts/register.html"
+
     def dispatch(self, request, *args: Any, **kwargs: Any):
         if request.user.is_authenticated:
-            return redirect('home:home')
+            return redirect("home:home")
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get(self, request):
         form = self.form_class()
-        return render(request,
-                      self.template_name,
-                      {'form': form}
-                    )
+        return render(request, self.template_name, {"form": form})
+
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(
-                cd['username'],
-                cd['email'],
-                cd['password']
-            )
-            messages.success(
-                request,
-                'Account created successfully',
-                'success'
-                )
-            return redirect('home:home')
-        return render(
-            request,
-            self.template_name,
-            {'form':form}
-            )
+            User.objects.create_user(cd["username"], cd["email"], cd["password"])
+            messages.success(request, "Account created successfully", "success")
+            return redirect("home:home")
+        return render(request, self.template_name, {"form": form})
 
 
 class UserLoginView(View):
-    form_class=UserLoginForm
-    template_name = 'accounts/login.html'
-    
+    form_class = UserLoginForm
+    template_name = "accounts/login.html"
+
     def dispatch(self, request, *args: Any, **kwargs: Any):
         if request.user.is_authenticated:
-            return redirect('home:home')
+            return redirect("home:home")
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get(self, request):
         form = self.form_class()
-        return render(
-            request,
-            self.template_name,
-            {'form': form}
-            )
-    
+        return render(request, self.template_name, {"form": form})
+
     def post(self, request):
         form = self.form_class(request.POST)
-        
+
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(
-                request,
-                username = cd['username'],
-                password = cd['password']
+                request, username=cd["username"], password=cd["password"]
             )
             if user is not None:
                 login(request, user)
-                messages.success(
-                    request,
-                    'Logged in successfully',
-                    'success'
-                )
-                return redirect('home:home')
-            
-            messages.error(
-                request,
-                'Invalid username or password.',
-                'warning'
-            )
-        return render(request, self.template_name, {'form': form})
-    
-    
-    
-class UserLogoutView(LoginRequiredMixin, View):      
+                messages.success(request, "Logged in successfully", "success")
+                return redirect("home:home")
+
+            messages.error(request, "Invalid username or password.", "danger")
+        return render(request, self.template_name, {"form": form})
+
+
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
-        messages.success(
-            request,
-            'Logged out successfully',
-            'success'
-        )
-        return redirect('home:home')
-    
+        messages.success(request, "Logged out successfully", "success")
+        return redirect("home:home")
+
+
 class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
-        user = User.objects.get(id = user_id)
-        return render(
-            request,
-            'accounts/profile.html',
-            {'user': user}
-            )
+        user = User.objects.get(id=user_id)
+        return render(request, "accounts/profile.html", {"user": user})
+
     def post(self, request):
         pass
-       
