4164d4dd6a0186c1ba19b2f015cc444e36a383c7
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-27 08:59:53 +0330

    url for posts created

--- Learning_Django/Mongard/Django_2/SocialNetwork/home/urls.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/home/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'home'
+app_name = "home"
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path("", views.HomeView.as_view(), name="home"),
+    path(
+        "posts/<int:post_id>/<slug:post_slug>",
+        views.PostDetailView.as_view(),
+        name="posts",
+    ),
 ]
