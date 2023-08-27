58ae2d3057e7c615ef472d2dc636af2fc36c6572
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-27 07:46:27 +0330

    feat: Session 16 Completed

--- Learning_Django/Mongard/Django_2/SocialNetwork/home/models.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/home/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     slug = models.SlugField()
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+
     def __str__(self) -> str:
-        return f'{self.id}-{self.slug}'
+        return f"{self.id} || {self.slug}"
