1756502fce3cdef203369a9315e63c4ccb6b3694
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-27 08:59:38 +0330

    feat: postdeatilview added

--- Learning_Django/Mongard/Django_2/SocialNetwork/home/views.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from .models import Post
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')    
+        posts = Post.objects.all()
+        return render(request, "home/index.html", {"posts": posts})
 
-    def post(self, request):
-        return render(request, 'home/index.html')
+
+class PostDetailView(View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(id=post_id, slug=post_slug)
+        return render(request, "home/post.html", {"post": post})
