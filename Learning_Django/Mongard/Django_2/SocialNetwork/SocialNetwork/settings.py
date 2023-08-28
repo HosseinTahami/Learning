1ea9dc6f71b563bc71a4c5249576a594dacb4892
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-28 06:21:12 +0330

    chore: smtp for google added

--- Learning_Django/Mongard/Django_2/SocialNetwork/SocialNetwork/settings.py
+++ Learning_Django/Mongard/Django_2/SocialNetwork/SocialNetwork/settings.py
@@ -130,3 +130,12 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "accounts.authenticate.EmailAuthenticationBackend",
 ]
+
+# Google SMTP
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "hosseintahami2000@gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = "xjuzkncvtkvxupphr"
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Hossein.Tahami.Corp"
