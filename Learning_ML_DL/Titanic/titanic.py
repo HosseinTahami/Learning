42eb0a4faf591407283bdf60eb0da4301c728802
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-08 07:52:30 +0330

    Feat: new files for ML

---
+++ Learning_ML_DL/Titanic/titanic.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+%matplotlib ipympl
+import matplotlib.pyplot as mpl
+from sklearn.linear_model import LogisticRegression
+
+
+data = pd.read_csv("titanic.csv")
+mpl.scatter(data["Age"], data["Survived"])
