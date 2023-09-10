9c9e2b23b63a8091348bc01b10a87df47ec6240d
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-10 14:21:34 +0330

    style: comment different way

--- Learning_Django/Mongard/Django_4/accounts/serializers.py
+++ Learning_Django/Mongard/Django_4/accounts/serializers.py
@@ -7,22 +7,24 @@ def clean_email(value):
         raise serializers.ValidationError("admin word should not be in email")
 
 
-# class UserRegisterSerializer(serializers.Serializer):
-#     username = serializers.CharField(required=True)
-#     email = serializers.EmailField(required=True, validators=[clean_email])
-#     password = serializers.CharField(required=True, write_only=True)
-#     confirm_password = serializers.CharField(required=True, write_only=True)
+"""
+class UserRegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True, validators=[clean_email])
+    password = serializers.CharField(required=True, write_only=True)
+    confirm_password = serializers.CharField(required=True, write_only=True)
 
-#     def validate_username(self, value):
-#         if value == "admin":
-#             raise serializers.ValidationError("username can not be 'admin'")
+    def validate_username(self, value):
+        if value == "admin":
+            raise serializers.ValidationError("username can not be 'admin'")
 
-#         return value
+        return value
 
-#     def validate(self, data):
-#         if data["password"] != data["confirm_password"]:
-#             raise serializers.ValidationError("passwords must match")
-#         return data
+    def validate(self, data):
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError("passwords must match")
+        return data
+"""
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
