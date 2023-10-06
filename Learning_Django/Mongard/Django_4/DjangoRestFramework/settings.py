7e12428debee4f3834d6d60e2934b175935fed09
HosseinTahami <hosseintahami2000@gmail.com>
2023-10-06 09:31:27 +0330

    feat: Session 18 Completed

--- Learning_Django/Mongard/Django_4/DjangoRestFramework/settings.py
+++ Learning_Django/Mongard/Django_4/DjangoRestFramework/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "home.apps.HomeConfig",
     "accounts.apps.AccountsConfig",
     "rest_framework",
-    'rest_framework.authtoken',
+    #"rest_framework.authtoken",
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -130,7 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.TokenAuthentication',
-        
+        #'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
--- Learning_Django/Mongard/Django_4/accounts/urls.py
+++ Learning_Django/Mongard/Django_4/accounts/urls.py
@@ -1,7 +1,11 @@
 # Django imports
 from django.urls import path
-from rest_framework.authtoken import views as auth_token
+#from rest_framework.authtoken import views as auth_token
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 # Inside Project Imports
 from . import views
@@ -10,7 +14,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
-    path("auth_token_creation/", auth_token.obtain_auth_token),
+    #path("auth_token_creation/", auth_token.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 router = routers.SimpleRouter()
