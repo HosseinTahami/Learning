2b6085c0030f6eea7d855666750cbcefc96d2726
HosseinTahami <hosseintahami2000@gmail.com>
2023-10-06 11:06:01 +0330

    feat: Session 20 Complete

--- Learning_Django/Mongard/Django_4/DjangoRestFramework/settings.py
+++ Learning_Django/Mongard/Django_4/DjangoRestFramework/settings.py
@@ -136,6 +136,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES':[
+        'rest_framework.renderers.AdminRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 }
 
 # DRF-SPECTACULAR SETTINGS
