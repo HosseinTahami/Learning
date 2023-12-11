9a64ad50976c68aac843d10c7c517f34266aeb87
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-11 12:40:04 +0330

    feat: base.html created

--- Learning_Django/Django.for./Beginners/Blog/blog/views.py
+++ Learning_Django/Django.for./Beginners/Blog/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
+class PostList(ListView):
+    model = Post
+    template_name = "home.html"
