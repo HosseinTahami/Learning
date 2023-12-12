8713efbc34b2de332e00ecdc59573c7a0f63016f
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-12 16:32:01 +0330

    feat: DetailView for post

--- Learning_Django/Django.for./Beginners/Blog/blog/views.py
+++ Learning_Django/Django.for./Beginners/Blog/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+
 class PostList(ListView):
     model = Post
     template_name = "home.html"
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = "post_detail.html"
