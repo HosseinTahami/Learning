9b620fccd0edb04b7818939ca016abccd0daac9a
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-12 16:46:35 +0330

    feat: post detail added

--- Learning_Django/Django.for./Beginners/Blog/blog/urls.py
+++ Learning_Django/Django.for./Beginners/Blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import PostList
+from .views import PostList, PostDetail
 
 urlpatterns = [
-    path("", PostList.as_view(), name="home")
+    path("", PostList.as_view(), name="home"),
+    path("post/<int:pk>/", PostDetail.as_view(), name="post_detail"),
 ]
