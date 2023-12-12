24a9989cb767566a0db1c2bb4e9c83489dc66e9d
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-12 19:03:01 +0330

    feat: say_hello updated

--- Learning_Django/Mosh/new/storfront/playground/views.py
+++ Learning_Django/Mosh/new/storfront/playground/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse("Hello, World !")
+    return render(request, "hello.html", {'name':"Hossein"})
