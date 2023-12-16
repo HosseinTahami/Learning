1ea7ca135965d3eb1305634eb74b2b3512a7cbb2
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 12:03:16 +0330

    feat: UserRegisterForm Completed

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/account/forms.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/account/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(label='UserName',widget=forms.TextInput(attrs={'placeholder':'@username', 'class':'form-control'}))
+    email = forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'placeholder':'YourMail@email.com', 'class':'form-control'}))
+    password = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'placeholder':'Password', 'class':'form-control'}))
 
