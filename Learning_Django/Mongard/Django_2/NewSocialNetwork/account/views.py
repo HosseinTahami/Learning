1130815fd238e98343b16821bd965ce6210b89d0
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 13:04:47 +0330

    feat: registration completed

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/account/views.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/account/views.py
@@ -1,6 +1,10 @@
-# Django imports
-from django.shortcuts import render
+""" Django Imports """
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+""" Projects Import """
 from .forms import UserRegisterForm
 
 class RegisterView(View):
@@ -9,4 +13,10 @@ class RegisterView(View):
         return render(request, 'account/register.html', {'form':form})
 
     def post(self, request):
-        ...
+        print("POST")
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(email=cd['email'], username=cd['username'], password=cd['password'])
+            messages.success(request, 'Registration Completed', 'success')
+            return redirect('home:home')
