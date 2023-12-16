f46149d60facafe43832bad67d4b8661e2b2be3b
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 16:12:46 +0330

    feat: clean for 2 passwords wrote

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/account/forms.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/account/forms.py
@@ -3,9 +3,42 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField(label='UserName',widget=forms.TextInput(attrs={'placeholder':'@username', 'class':'form-control'}))
-    email = forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'placeholder':'YourMail@email.com', 'class':'form-control'}))
-    password = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'placeholder':'Password', 'class':'form-control'}))
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'@username',
+                'class':'form-control'
+                }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder':'YourMail@email.com',
+                'class':'form-control'
+            }
+        )
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':'Password',
+                'class':'form-control',
+            }
+        )
+    )
+    confirm_password = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':'Confirm Password',
+                'class':'form-control'
+            }
+        )
+    )
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -21,3 +54,9 @@ class UserRegisterForm(forms.Form):
             raise ValidationError('This email is already used !')
         return email
     
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError("passwords Do not match !")
