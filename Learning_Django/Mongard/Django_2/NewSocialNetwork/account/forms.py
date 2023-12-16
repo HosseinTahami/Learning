2ad3fcaf977490433bf40a4c2bea0b57226f0b2d
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 15:21:57 +0330

    feat: clean_email & clean_username

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/account/forms.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/account/forms.py
@@ -1,7 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserRegisterForm(forms.Form):
     username = forms.CharField(label='UserName',widget=forms.TextInput(attrs={'placeholder':'@username', 'class':'form-control'}))
     email = forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'placeholder':'YourMail@email.com', 'class':'form-control'}))
     password = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'placeholder':'Password', 'class':'form-control'}))
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError("This username already taken !")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This email is already used !')
+        return email
+    
