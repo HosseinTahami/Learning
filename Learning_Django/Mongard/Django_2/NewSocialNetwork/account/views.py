07a1e01d3f2b59c696d6d47e2b1e1d4a8dc63a88
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 11:29:43 +0330

    feat: get in RegisterView updated

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/account/views.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/account/views.py
@@ -1,10 +1,12 @@
 # Django imports
 from django.shortcuts import render
 from django.views import View
+from .forms import UserRegisterForm
 
 class RegisterView(View):
     def get(self, request):
-        ...
-    
+        form = UserRegisterForm()
+        return render(request, 'account/register.html', {'form':form})
+
     def post(self, request):
         ...
