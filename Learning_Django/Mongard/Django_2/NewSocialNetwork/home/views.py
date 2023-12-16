4b30549de20d08c77fa482fe6173d4805c4b3860
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 11:06:54 +0330

    refactor: fbv --> cbv

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/home/views.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/home/views.py
@@ -1,4 +1,10 @@
+# Django imports
 from django.shortcuts import render
+from django.views import View
 
-def home(request):
-    return render(request,'home/index.html')
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/index.html')
+    
+    def post(self, request):
+        return render(request, 'home/index.html')
