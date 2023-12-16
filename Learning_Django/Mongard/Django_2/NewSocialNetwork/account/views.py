3d172670cfe7baf1ad00a37ab3460241b4d8b495
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-16 20:31:44 +0330

    feat: can not access logout without login

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/account/views.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/account/views.py
@@ -7,6 +7,7 @@ from django.views import View
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 """ Projects Import """
 from .forms import (
@@ -67,7 +68,7 @@ class UserLoginView(View):
             messages.error(request,'Login Failed', 'danger')
             return render(request, self.template_name, {'form':form} )
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         messages.success(request, 'Logout Successfully', 'success')
