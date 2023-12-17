92b4a2ede00c451772c8d12a6db06cae4cd7f4f4
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-17 15:38:32 +0330

    feat: get_absolute_url added

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/home/models.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +11,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return f'{self.slug}'
+    
+    def get_absolute_url(self):
+        return reverse('home:post_detail', args=(self.id, self.slug,))
