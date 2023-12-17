22a2a2e9edb2a904334cfc9a49cff4b29e12c1f8
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-17 17:14:58 +0330

    refactor: form name changed

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/home/forms.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/home/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Post
 
-class PostUpdateForm(forms.ModelForm):
+class PostCreateUpdateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('body',)
