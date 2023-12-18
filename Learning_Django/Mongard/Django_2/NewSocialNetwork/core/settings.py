a36798b1f12b85220689d414bb5fba4095c1f2f0
HosseinTahami <hosseintahami2000@gmail.com>
2023-12-18 14:41:46 +0330

    feat: SMTP added

--- Learning_Django/Mongard/Django_2/NewSocialNetwork/core/settings.py
+++ Learning_Django/Mongard/Django_2/NewSocialNetwork/core/settings.py
@@ -124,3 +124,12 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authenticate.AuthenticationBackend'
 ]
+
+# Google Account
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'hosseintahami2000@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'cblrvkbgkfhfnsti'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Hossein-Tahami-Social-App'
