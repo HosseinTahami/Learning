d6d1bf83a0d3ee08b941aad7f2822cec4805e5c4
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-01 14:55:45 +0330

    sqlite to mysql changed

--- Learning_Django/Mosh/storefront/storefront/settings.py
+++ Learning_Django/Mosh/storefront/storefront/settings.py
@@ -84,10 +84,20 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'iran1379'
     }
 }
 
