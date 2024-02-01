fbcb94b8da814b96af198e975563a90274939194
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-01 19:03:53 +0330

    playing with queries

--- Learning_Django/Mosh/storefront/playground/views.py
+++ Learning_Django/Mosh/storefront/playground/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Order
+from store.models import Product, Order, OrderItem
+from django.db.models import Q, F
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(unit_price__range=((20, 100)))
-    return render(request, 'hello.html', {'name': 'Hossein', 'products': list(queryset)})
+    queryset_1 = Product.objects.filter(unit_price__range=((20, 100)))
+    queryset_2 = Product.objects.filter(
+        title__icontains="Cof").order_by('title')
+    queryset_3 = Order.objects.filter(
+        orderitem__product__title__icontains="coff").order_by('id').reverse()
+
+    # Q--> or:| , and:& , ~:not
+    queryset_4 = Product.objects.filter(
+        Q(inventory__lt=10) | Q(unit_price__lt=20)).order_by('-title', 'unit_price')
+    queryset_5 = Product.objects.filter(
+        Q(inventory__lt=10) & Q(unit_price__lt=20))
+    queryset_6 = Product.objects.filter(
+        Q(inventory__lt=10) | ~Q(unit_price__lt=20)).earliest('unit_price')
+    queryset_7 = Product.objects.filter(inventory=F('unit_price'))
+    queryset_8 = Product.objects.filter(
+        unit_price__gt=F('collection__id')).latest('unit_price')
+    queryset_9 = Product.objects.values(
+        'id',
+        'title',
+        'collection__title'
+    )
+    queryset_9 = Product.objects.values_list(
+        'id',
+        'title',
+        'collection__title'
+    )
+
+    # related_name and related_query_name
+    queryset_10 = Product.objects.filter(
+        orderitem__isnull=False).order_by('title').distinct()
+
+    # distinct --> removes the duplicates
+    queryset_11 = OrderItem.objects.values('order_id').distinct()
+    queryset_12 = Product.objects.filter(id__in=queryset_11).order_by('title')
+
+    return render(
+        request,
+        'hello.html',
+        {
+            'name': 'Hossein',
+            'products': list(queryset_10)
+        }
+    )
--- Learning_Django/Mosh/storefront/store/models.py
+++ Learning_Django/Mosh/storefront/store/models.py
@@ -73,7 +73,8 @@ class Address(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
