168e826d67cf87225dcfc7513cf3047d15e3b0c4
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-01 11:47:43 +0330

    tags app completed

--- Learning_Django/Mosh/storefront/tags/models.py
+++ Learning_Django/Mosh/storefront/tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=250)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type ( product, video, article, post, music, ...)
+    # ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
