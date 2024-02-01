06092b8eba790effb306aa600274a539e7aa956d
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-01 11:47:19 +0330

    store models completed

--- Learning_Django/Mosh/storefront/store/models.py
+++ Learning_Django/Mosh/storefront/store/models.py
@@ -1,3 +1,88 @@
 from django.db import models
 
-# Create your models here.
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=250)
+    discount = models.FloatField()
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+    # related_name = 'products' | 'product_set'
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=250)
+    featured_product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, related_name='collections')
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=True)
+    phone = models.CharField(max_length=12)
+    membership = models.CharField(
+        max_length=1,
+        choices=MEMBERSHIP_CHOICES,
+        default=MEMBERSHIP_BRONZE
+    )
+
+
+class Order(models.Model):
+    PENDING_STATUS = 'P'
+    COMPLETE_STATUS = 'C'
+    FAILED_STATUS = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PENDING_STATUS, 'Pending'),
+        (COMPLETE_STATUS, 'Complete'),
+        (FAILED_STATUS, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    placement_status = models.CharField(
+        max_length=1,
+        choices=PAYMENT_STATUS_CHOICES,
+        default=PENDING_STATUS
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=250)
+    city = models.CharField(max_length=250)
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+    )
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
