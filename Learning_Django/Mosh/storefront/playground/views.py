69e214edf2cb3148ebb71675fe33a310d81f3850
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-01 16:39:03 +0330

    show products with filter

--- Learning_Django/Mosh/storefront/playground/views.py
+++ Learning_Django/Mosh/storefront/playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product, Order
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Hossein'})
+    queryset = Product.objects.filter(unit_price__range=((20, 100)))
+    return render(request, 'hello.html', {'name': 'Hossein', 'products': list(queryset)})
