9666e9b294e9dae955b5c5a5e8b021b41489451e
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-06 08:12:34 +0330

    .

--- Learning_Django/Mongard/Django_1/home/views.py
+++ Learning_Django/Mongard/Django_1/home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def say_hello(request):
+    person = {
+        'name': 'admin'
+    }
+    return render(request, 'hello.html', person)
