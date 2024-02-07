6313f7383fb16a604ce1005e8628316fb57d60d2
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-07 09:42:52 +0330

    user register, login, logout

---
+++ Learning_Django/Mongard/Django_1/accounts/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
