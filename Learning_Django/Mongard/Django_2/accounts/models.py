bd81da6a68f3f08b74c9f3f9b209aa5d102ae19e
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-08 17:44:06 +0330

    Relation model created

--- Learning_Django/Mongard/Django_2/accounts/models.py
+++ Learning_Django/Mongard/Django_2/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Relation(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followees')
+    followee = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followers'
+    )
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return (f'{self.follower} --> {self.followee}')
