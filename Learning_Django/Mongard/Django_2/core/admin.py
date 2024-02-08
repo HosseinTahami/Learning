af1fab00613e570665264f6a67a95770a02be05b
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-08 10:44:16 +0330

    Customize Admin Panel

--- Learning_Django/Mongard/Django_2/core/admin.py
+++ Learning_Django/Mongard/Django_2/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'slug', 'updated']
+    search_fields = ['slug']
+    sortable_by = ['user']
+    list_filter = ['updated', 'created']
+    prepopulated_fields = {
+        'slug': ['body'],
+    }
+    raw_id_fields = ['user']
