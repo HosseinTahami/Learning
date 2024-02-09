481700fdc817c6161eb9d91eb1cfab81d6eb822b
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-09 10:30:23 +0330

    User model created

--- Learning_Django/Mongard/Django_3/accounts/models.py
+++ Learning_Django/Mongard/Django_3/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=12)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField()
+    birth_date = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['username', 'email']
+
+    def __str__(self):
+        return f'{self.email}'
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
