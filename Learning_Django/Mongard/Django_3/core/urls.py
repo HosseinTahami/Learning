32c2b6d41056562a388b1923ff00d3e3b4fff53e
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-12 19:41:26 +0330

    chore: view's urls added

--- Learning_Django/Mongard/Django_3/core/urls.py
+++ Learning_Django/Mongard/Django_3/core/urls.py
@@ -1,8 +1,15 @@
+# Django Imports
+
 from django.urls import path
 
+# Project Imports
+
+from . import views
 
 app_name = 'core'
 
 urlpatterns = [
+
+    path('', views.HomePageView.as_view(), name='home'),
 
 ]
