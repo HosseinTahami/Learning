ae7b107f5d645268b9a2c0ea65e031edc426730d
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-12 19:41:42 +0330

    feat: HomePageView added

--- Learning_Django/Mongard/Django_3/core/views.py
+++ Learning_Django/Mongard/Django_3/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# Django Imports
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+# Project Imports
+
+
+class HomePageView(View):
+
+    def get(self, request):
+        return render(request, 'core/home.html')
