f89729dbcd0d04c5428e01273e64b0ba7868ba64
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-12 19:34:22 +0330

    chore: register url added

--- Learning_Django/Mongard/Django_3/accounts/urls.py
+++ Learning_Django/Mongard/Django_3/accounts/urls.py
@@ -1,8 +1,16 @@
+# Django Imports
+
 from django.urls import path
+
+# Project Imports
+
+from . import views
 
 
 app_name = 'accounts'
 
 urlpatterns = [
+
+    path('register/', views.UserRegisterView.as_view(), name='user_register'),
 
 ]
