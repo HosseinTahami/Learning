50455b3b9412fe6b8f36611d7b5631d55f8da999
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-14 13:46:43 +0330

    feat: username deleted from user

--- Learning_Django/Mongard/Django_3/accounts/managers.py
+++ Learning_Django/Mongard/Django_3/accounts/managers.py
@@ -3,13 +3,10 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, phone_number, email, username, first_name, last_name, password):
+    def create_user(self, phone_number, email, first_name, last_name, password):
 
         if not phone_number:
             raise ValueError("Phone number is mandatory")
-
-        if not username:
-            raise ValueError("Username is mandatory")
 
         if not email:
             raise ValueError("Email is mandatory")
@@ -23,7 +20,6 @@ class UserManager(BaseUserManager):
         user = self.model(
             phone_number=phone_number,
             email=self.normalize_email(email),
-            username=username,
             first_name=first_name,
             last_name=last_name,
         )
@@ -32,12 +28,11 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phone_number, username, email, first_name, last_name, password):
+    def create_superuser(self, phone_number,  email, first_name, last_name, password):
 
         user = self.create_user(
             phone_number,
             email,
-            username,
             first_name,
             last_name,
             password,
