82b2f28d04b8aa743b440d34acc4ba2cdd2473aa
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-14 11:44:40 +0330

    feat: verify added to urls

--- Learning_Django/Mongard/Django_3/accounts/urls.py
+++ Learning_Django/Mongard/Django_3/accounts/urls.py
@@ -12,5 +12,6 @@ app_name = 'accounts'
 urlpatterns = [
 
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
+    path('verify/', views.UserRegisterVerifyCodeView.as_view(), name='verify_code')
 
 ]
