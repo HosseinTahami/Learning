4b6c9adc5c07994aebbbe3c89375e892282f62c6
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-14 14:21:56 +0330

    feat: category and product model added

--- Learning_Django/Mongard/Django_3/core/models.py
+++ Learning_Django/Mongard/Django_3/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['-name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name', 'available']
