a700e5583248b73f8f877d5f5703c68679fb6ca5
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-15 15:36:03 +0330

    refactor: better comments

--- Learning_RabbitMQ/Session3/sender.py
+++ Learning_RabbitMQ/Session3/sender.py
@@ -1,29 +1,35 @@
 import pika
 
-credentials = pika.PlainCredentials('root', 'iran1379')
 
 ''' First we are creating a connection'''
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(
         host='localhost',
-        credentials=credentials
-        )
     )
+)
 
-''' create a channel for the connection '''
+''' Create a channel for the connection '''
+
 channel = connection.channel()
 
 
-''' create a queue or declare which queue is the one we want !'''
-''' queue_declare will create a queue if needed o
+''' Create a queue or declare which queue is the one we want !
+    queue_declare will create a queue if needed
     or connect to a queue if there is one
 '''
 channel.queue_declare(queue='one')
 
+
+''' basic_publish will publish sth inside the channel we have.
+    exchange argument is for the type of our exchange method
+    routing_key argument is for the queue name
+    body argument is the thing we want to publish
+'''
 channel.basic_publish(
     exchange='',
     routing_key='one',
     body='hello world !'
-    )
+)
 
+'''We always have to close our connection'''
 connection.close()
