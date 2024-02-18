9dca430f16aba3e796565b79635c009df0a26718
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-18 19:53:32 +0330

    feat: topic exchange sender

---
+++ Learning_RabbitMQ/Session9/sender.py
@@ -0,0 +1,37 @@
+import pika
+
+credentials = pika.PlainCredentials(
+    username='root',
+    password='iran1379'
+)
+
+parameters = pika.ConnectionParameters(
+    host='localhost',
+    credentials=credentials
+)
+
+connection = pika.BlockingConnection(
+    parameters=parameters
+)
+
+channel = connection.channel()
+
+channel.exchange_declare(
+    exchange_type='topic',
+    exchange='logs'
+)
+
+messages = {
+    'error.warning.important': 'important message',
+    'info.debug.unimportant': 'unimportant message',
+}
+
+for key, value in messages.items():
+    channel.basic_publish(
+        exchange='topics',
+        routing_key='k',
+        body=value
+    )
+
+print('Message Sent..!')
+connection.close()
