f54f75e8690f8b38a446a97dbacff280e9f4ef79
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-19 17:59:36 +0330

    feat: celery added

--- Learning_Django/Mongard/Django_3/config/celery_config.py
+++ Learning_Django/Mongard/Django_3/config/celery_config.py
@@ -3,7 +3,21 @@ from datetime import timedelta
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-celery_app = Celery('Config')
+
+celery_app = Celery(
+    main='config',
+    backend='rpc://',
+    broker='amqp://guest:guest@localhost:5672'
+)
+
 celery_app.autodiscover_tasks()
 
-# celery_app.conf.broker_url =
+celery_app.conf.update(
+    task_serializer='json',
+    result_serializer='pickle',
+    accept_content=['json', 'pickle'],
+    result_expires=timedelta(days=1),
+    task_always_eager=False,
+    worker_prefetch_multiplier=2,
+
+)
