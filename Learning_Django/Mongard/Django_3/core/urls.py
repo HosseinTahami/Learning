d7a322e334665d0c00d8ca9ed105ed619454737f
Hossein Tahami <hosseintahami2000@gmail.com>
2024-02-19 18:00:46 +0330

    chore: bucket url added

--- Learning_Django/Mongard/Django_3/core/urls.py
+++ Learning_Django/Mongard/Django_3/core/urls.py
@@ -11,6 +11,7 @@ app_name = 'core'
 urlpatterns = [
 
     path('', views.HomePageView.as_view(), name='home'),
+    path('bucket/', views.BucketHome.as_view(), name='bucket'),
     path('<slug:product_slug>/',
          views.ProductDetailView.as_view(), name='product_detail'),
 ]
