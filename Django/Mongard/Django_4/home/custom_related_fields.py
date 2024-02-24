c6287f7eb7bf03c5a52ad42e662d37f84ad1e575
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-24 15:59:55 +0330

    Refactor: Delete old files

--- Django/Mongard/Django_4/home/custom_related_fields.py
+++
@@ -1,8 +0,0 @@
-'''custom_related_fields'''
-
-from rest_framework import serializers
-
-class UsernameEmailRelationalField(serializers.RelatedField):
-    
-    def to_representation(self, value):
-        return f'{value.username} || {value.email}'
