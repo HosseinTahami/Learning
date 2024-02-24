2e007bb9879ea7d697182631b355c93dd07599e4
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-24 22:19:46 +0330

    Feat: Create Person Model

--- Django/Mongard/Django_4/home/models.py
+++ Django/Mongard/Django_4/home/models.py
@@ -1,3 +1,12 @@
+# Django Imports
+
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.SmallIntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'Name:{self.name}'
