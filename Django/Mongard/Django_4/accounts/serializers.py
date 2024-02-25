b60087107e6fa68ed7c5605dc5ad9d23800255d0
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-25 22:07:39 +0330

    Feat: Add validate_username for UserRegisterSerializer

--- Django/Mongard/Django_4/accounts/serializers.py
+++ Django/Mongard/Django_4/accounts/serializers.py
@@ -8,3 +8,8 @@ class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, username):
+        if username == 'admin':
+            raise serializers.ValidationError("username can not be 'admin'..!")
+        return username
