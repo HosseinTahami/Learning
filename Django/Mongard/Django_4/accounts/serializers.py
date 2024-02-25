ecfb9242d773c6b0d1c718a51ecb8ee3b4923e42
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-25 22:25:31 +0330

    Feat: Add Validator for password & confirm_password

--- Django/Mongard/Django_4/accounts/serializers.py
+++ Django/Mongard/Django_4/accounts/serializers.py
@@ -2,10 +2,12 @@
 
 from rest_framework import serializers
 
+
 ''' First Type of Validation in DRF
     Put the name of function inside a list
     and send it in the specific field as an
-    Argument named 'validator' '''
+    Argument named 'validator'
+'''
 
 
 def clean_email(email):
@@ -20,7 +22,23 @@ class UserRegisterSerializer(serializers.Serializer):
     password = serializers.CharField(required=True, write_only=True)
     confirm_password = serializers.CharField(required=True, write_only=True)
 
+    ''' Second Type of validator is a function 
+        inside the class and it is called validate_fieldname
+    '''
+
     def validate_username(self, username):
         if username == 'admin':
             raise serializers.ValidationError("username can not be 'admin'..!")
         return username
+
+    ''' Third Type of validators is a function with the name
+        of validate and does not belong to any specific field
+    '''
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if password != confirm_password:
+            raise serializers.ValidationError("Passwords must match..!")
+        return data
