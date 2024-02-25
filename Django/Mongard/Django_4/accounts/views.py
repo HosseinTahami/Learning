c55b8c17a75310e430bdf6e0d61c137e9ec6ada6
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-25 21:51:17 +0330

    Feat: Add UserRegisterView

--- Django/Mongard/Django_4/accounts/views.py
+++ Django/Mongard/Django_4/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# REST Framework Imports
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Django Imports
+
+from django.contrib.auth.models import User
+
+# Inside Project Imports
+
+from .serializers import UserRegisterSerializer
+
+
+class UserRegisterView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        ser_data = UserRegisterSerializer(data=request.POST)
+        if ser_data.is_valid():
+            vd = ser_data.validated_data
+            User.objects.create_user(
+                username=vd['username'],
+                email=vd['email'],
+                password=vd['password']
+            )
+            return Response(ser_data.data)
+        return Response(ser_data.errors)
