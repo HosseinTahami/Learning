ed8b66e72c61102dbd46e7e2a04b7abfab04d011
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-25 22:22:23 +0330

    Feat: Add Validator for email Field

--- Django/Mongard/Django_4/accounts/serializers.py
+++ Django/Mongard/Django_4/accounts/serializers.py
@@ -2,12 +2,23 @@
 
 from rest_framework import serializers
 
+''' First Type of Validation in DRF
+    Put the name of function inside a list
+    and send it in the specific field as an
+    Argument named 'validator' '''
+
+
+def clean_email(email):
+    if 'admin' in email:
+        raise serializers.ValidationError("'admin' can not be in email..!")
+
 
 class UserRegisterSerializer(serializers.Serializer):
 
     username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=True, validators=[clean_email])
     password = serializers.CharField(required=True, write_only=True)
+    confirm_password = serializers.CharField(required=True, write_only=True)
 
     def validate_username(self, username):
         if username == 'admin':
