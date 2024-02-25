995efd4d43a9aaced4a087deacf1883863cb8cb3
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-25 21:51:44 +0330

    Feat: Add accounts app url

--- Django/Mongard/Django_4/config/urls.py
+++ Django/Mongard/Django_4/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
+    path('account/', include('accounts.urls', namespace='accounts')),
 ]
