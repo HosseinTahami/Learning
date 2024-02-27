966c702754c4c26a9a3ebda0cb785cc0d81a73ca
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-27 23:27:33 +0330

    Feat: Authentication from setting.py

--- Django/Mongard/Django_4/config/settings.py
+++ Django/Mongard/Django_4/config/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework Authentication
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.BasicAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#     ]
+# }
