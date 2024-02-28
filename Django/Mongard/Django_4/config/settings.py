7f3cae0a7dd1029b1951c6f58a1fd9f89d098fb4
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-28 18:01:48 +0330

    Feat: Implement TokenAuthentication

--- Django/Mongard/Django_4/config/settings.py
+++ Django/Mongard/Django_4/config/settings.py
@@ -126,10 +126,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST Framework Authentication
+'''Implementing Authentication via this way, all views will be affected..!'''
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.BasicAuthentication',
-#         'rest_framework.authentication.SessionAuthentication',
-#     ]
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        #        'rest_framework.authentication.BasicAuthentication',
+        #       'rest_framework.authentication.SessionAuthentication',
+    ]
+}
