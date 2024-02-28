92ff4f86423c84d82840770b4ae8484db456d209
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-28 22:03:04 +0330

    Chore: Set COERCE_DECIMAL_TO_STRING to False

--- Django/Mosh/storefront2/store/views.py
+++ Django/Mosh/storefront2/store/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Rest Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        return Response('OK')
+
+    if request.method == 'POST':
+        ...
+
+
+@api_view(['GET', 'POST'])
+def product_detail(request, *args, **kwargs):
+    return Response(kwargs['product_id'])
