0f9b6d8ad2a220864265469ac7f2d551d26e8a30
HosseinTahami <hosseintahami2000@gmail.com>
2024-06-09 00:11:15 +0330

    Feat: Session 11 Complete

---
+++ Python/Mongard/Course/11.py
@@ -0,0 +1,15 @@
+"""
+When there is only one item in tuples use (,) at the end !
+"""
+names = ('Hossein',)
+print(type(names))
+
+name = ('Hossein')
+print(type(name))
+
+numbers = (1, 2, 5, 6, 23)
+print(type(numbers))
+
+"""
+You can't change tuples !
+"""
