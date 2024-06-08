95b00c29bbafbf3b601cb93b45ffa9db70ca3b04
HosseinTahami <hosseintahami2000@gmail.com>
2024-06-08 23:26:47 +0330

    Feat: Session 8 Completed

---
+++ Python/Mongard/Course/8.py
@@ -0,0 +1,26 @@
+names = ['Hossein', 'Kevin', 'Bob', 'Admin', 'Ali', 'Tahami']
+
+# Continue
+for name in names:
+    if name == 'Admin':
+        continue
+    print(name)
+else:
+    print("End...\n")
+
+
+# Break
+for name in names:
+    if name == 'Admin':
+        break
+    print(name)
+else:
+    print("End...")
+
+# Pass
+
+while True:
+    ...
+
+while True:
+    pass
