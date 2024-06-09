788842481757379df812ff393aad64eeb89058f2
HosseinTahami <hosseintahami2000@gmail.com>
2024-06-09 15:41:39 +0330

    Feat: Session 29 Complete !

---
+++ Python/Mongard/Course/29.py
@@ -0,0 +1,19 @@
+"""
+This module contains Person Class !
+"""
+
+
+class Person:
+    """
+    This Class responsible for handling person objects !
+    """
+
+    def show(self):
+        ...
+    """
+    This function will ...
+    """
+
+
+print(Person.__doc__)
+# help(Person)
