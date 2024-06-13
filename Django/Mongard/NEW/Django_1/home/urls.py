845f801b116a5f27ef25e0caa02d36aba76bf16e
HosseinTahami <hosseintahami2000@gmail.com>
2024-06-13 08:13:04 +0330

    Feat: Session 1 to 12 Complete !

--- Django/Mongard/NEW/Django_1/home/urls.py
+++ Django/Mongard/NEW/Django_1/home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello')
+    path('todos/', views.show_todos, name='show_todos'),
+    path('todos/<int:todo_id>', views.show_todo_detail, name='todo_detail'),
 ]
--- Django/Mongard/NEW/Django_1/home/views.py
+++ Django/Mongard/NEW/Django_1/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Todo
 
 
-def hello(request):
-    return HttpResponse("Hello")
+def show_todos(request):
+    todos = Todo.objects.all()
+    return render(request, 'home.html', {'todos': todos})
+
+
+def show_todo_detail(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    return render(request, 'todo_detail.html', {'todo': todo})
