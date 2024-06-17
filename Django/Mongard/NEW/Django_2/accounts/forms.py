398f172ed36219723c8c75ca7207214109830228
Hossein Tahami <hosseintahami2000@gmail.com>
2024-06-17 10:57:25 +0330

    Feat: Register Page Complete !

--- Django/Mongard/NEW/Django_2/accounts/forms.py
+++ Django/Mongard/NEW/Django_2/accounts/forms.py
@@ -3,7 +3,40 @@ from django import forms
 
 class RegistrationForm(forms.Form):
 
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
-    confirm_password = forms.CharField()
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your Username"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "example@email.com"
+            }
+
+        )
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "*******",
+            }
+        )
+    )
+    confirm_password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "*******",
+            }
+        )
+    )
