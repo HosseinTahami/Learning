35192d5dcfb80080c2fb19c76bc2b6987f37eeea
Hossein Tahami <hosseintahami2000@gmail.com>
2024-06-20 16:50:14 +0330

    Feat: Complete Admin Panel for Post Model

--- Django/Mongard/NEW/Django_2/posts/admin.py
+++ Django/Mongard/NEW/Django_2/posts/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Post)
+# admin.site.register(models.Post)
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug',)
+    list_filter = ('updated',)
+    prepopulated_fields = {
+        'slug': ('body',)
+    }
+    raw_id_fields = ('user',)
+
+
+#admin.site.register(models.Post, PostAdmin)
