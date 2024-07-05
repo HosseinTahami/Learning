4c62ee918255eada226b4b3967422129453b37b5
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-05 06:27:57 +0330

    Feat: Add docstring

--- FastAPI/First/agent.py
+++ FastAPI/First/agent.py
@@ -11,3 +11,25 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("agent:app", reload=True)
+
+"""
+$ http -v localhost:8000/agent who:hossein
+
+    GET /agent HTTP/1.1
+    Accept: */*
+    Accept-Encoding: gzip, deflate
+    Connection: keep-alive
+    Host: localhost:8000
+    User-Agent: HTTPie/3.2.2
+    who: hossein
+
+
+
+    HTTP/1.1 200 OK
+    content-length: 14
+    content-type: application/json
+    date: Fri, 05 Jul 2024 02:56:08 GMT
+    server: uvicorn
+
+    "HTTPie/3.2.2"
+"""
