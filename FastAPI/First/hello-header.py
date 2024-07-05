9b814bf23aef9becb340374fced31dc782260900
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-05 06:24:16 +0330

    Feat: add docstring

--- FastAPI/First/hello-header.py
+++ FastAPI/First/hello-header.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/agent")
 def greet(who:str = Header()):
     return f"Hello? {who}?"
+
+"""
+$ http localhost:8000/agent who:hossein 
+
+    HTTP/1.1 200 OK
+    content-length: 17
+    content-type: application/json
+    date: Fri, 05 Jul 2024 02:53:33 GMT
+    server: uvicorn
+
+    "Hello? hossein?"
+"""
