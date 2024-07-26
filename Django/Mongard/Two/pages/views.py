b90f289abcc4c7c501a809cb8dba030d6b06e440
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-26 10:29:03 +0330

    Feat: Create HomePageView class

--- Django/Mongard/Two/pages/views.py
+++ Django/Mongard/Two/pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class HomePageView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name="pages/home.html")
