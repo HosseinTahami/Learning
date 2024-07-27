fc504b242b19acd393c7fb78cef5ea4308cc52b5
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-27 18:17:53 +0330

    Chore: Add apps urls

--- Django/Mongard/Two/core/urls.py
+++ Django/Mongard/Two/core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("pages.urls")),
+    path("", include("pages.urls", namespace="pages")),
+    path("posts/", include("posts.urls", namespace="posts")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
 ]
