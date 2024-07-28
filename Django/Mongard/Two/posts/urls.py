5952baa985717a3aff3c8694b866bf5451848f3c
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-28 20:39:02 +0330

    Chore: Add new urls

--- Django/Mongard/Two/posts/urls.py
+++ Django/Mongard/Two/posts/urls.py
@@ -6,5 +6,8 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
+    path("<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
     path("<slug:post_slug>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("post/create/", views.PostCreateView.as_view(), name="post_create"),
 ]
