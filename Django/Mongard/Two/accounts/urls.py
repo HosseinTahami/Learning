4555ef1302270e377c0800879c5667ed45922cd9
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-28 20:37:51 +0330

    Feat: Add password reset urls

--- Django/Mongard/Two/accounts/urls.py
+++ Django/Mongard/Two/accounts/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
     path("profile/<int:pk>/", views.UserProfileView.as_view(), name="profile"),
+    # Reset Password
+    path("password/reset/", views.UserPasswordResetView.as_view(), name="password_reset"),
+    path("password/reset/done/", views.UserPasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password/reset/confirm/<uidb64>/<token>/", views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("password/reset/complete/", views.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
     
 ]
