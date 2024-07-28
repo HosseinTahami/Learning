9c15fb6f0a6eba707d27b0eff49931825db3154e
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-28 20:38:23 +0330

    Feat: Add Email SMTP configurations

--- Django/Mongard/Two/core/settings.py
+++ Django/Mongard/Two/core/settings.py
@@ -143,3 +143,11 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "accounts.backends.EmailBackend",
 ]
+
+# SMTP
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "hosseintahami2000@gmail.com"
+EMAIL_HOST_PASSWORD = "sokixsyeckzuhxjh"
+DEFAULT_FROM_EMAIL = "Django Blog Website"
+EMAIL_USE_TLS = True
