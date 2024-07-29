cd5724e56c74c2a1e764526093b7e66799b0e5b3
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-30 01:44:57 +0330

    Feat: Relation model in Admin

--- Django/Mongard/Two/accounts/admin.py
+++ Django/Mongard/Two/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Relation
+
+
+@admin.register(Relation)
+class RelationAdmin(admin.ModelAdmin):
+    list_display = ["follower", "following", "created"]
+    raw_id_fields = ["follower", "following"]
+    list_filter = ["created"]
+    
