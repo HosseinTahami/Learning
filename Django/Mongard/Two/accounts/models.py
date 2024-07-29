ea5f62873d8c36869f15fd69d95876295e03779b
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-30 01:45:10 +0330

    Feat: Create Relation Model

--- Django/Mongard/Two/accounts/models.py
+++ Django/Mongard/Two/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Relation(models.Model):
+
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower} starts following {self.following}"
