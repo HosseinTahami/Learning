d352197bbb41bd8f82defadb84e1ba8e54beaa1f
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-30 14:35:07 +0330

    Feat: Add Comments Model

--- Django/Mongard/Two/posts/models.py
+++ Django/Mongard/Two/posts/models.py
@@ -30,3 +30,17 @@ class Post(models.Model):
 
     class meta:
         ordering = ["created", "title"]
+
+class Comment(models.Model):
+
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
+    title = models.CharField(max_length=127)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name="comment_comments", blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comments")
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} = {self.body[:20]}'
+    
