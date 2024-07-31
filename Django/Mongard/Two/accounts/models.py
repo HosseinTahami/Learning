539fcdc0fe86c319e639f916298aba3bd1c93204
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-31 07:30:26 +0330

    Feat: Extend User Model

--- Django/Mongard/Two/accounts/models.py
+++ Django/Mongard/Two/accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+
 from django.db import models
 
 User = get_user_model()
@@ -6,9 +7,25 @@ User = get_user_model()
 
 class Relation(models.Model):
 
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.follower} starts following {self.following}"
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField(null=True)
+    bio = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"{self.age}"
+
+
