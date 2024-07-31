9e0e16aa9612290f19e9ed8c5143527994c9033e
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-31 07:27:18 +0330

    Chore: Add new urls

--- Django/Mongard/Two/posts/urls.py
+++ Django/Mongard/Two/posts/urls.py
@@ -6,8 +6,16 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
+    path("search/", views.PostSearchView.as_view(), name="post_search"),
+    path("create/", views.PostCreateView.as_view(), name="post_create"),
     path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
     path("<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
     path("<slug:post_slug>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("create/", views.PostCreateView.as_view(), name="post_create"),
+    path("<int:post_pk>/like/", views.PostLikeView.as_view(), name="post_like"),
+    path(
+        "<int:post_pk>/comment/<int:comment_pk>/reply/",
+        views.ReplyCommentView.as_view(),
+        name="reply_comment",
+    ),
+    
 ]
