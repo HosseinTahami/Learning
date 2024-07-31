2ecaafe821004a2b49fc055695db202c2894d8bf
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-31 09:06:39 +0330

    Feat: Add HomePageView

--- Django/Mongard/Three/pages/views.py
+++ Django/Mongard/Three/pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomePageView(View):
+
+    template_name = "pages/home.html"
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
