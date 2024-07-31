c01bb12b9ea6436af661187f1ab93fe5fbce8601
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-31 07:31:39 +0330

    Feat: Change Admin Behavior for User Model

--- Django/Mongard/Two/accounts/admin.py
+++ Django/Mongard/Two/accounts/admin.py
@@ -1,11 +1,25 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 from django.contrib import admin
 
-from .models import Relation
+from .models import Relation, Profile
 
+User = get_user_model()
 
 @admin.register(Relation)
 class RelationAdmin(admin.ModelAdmin):
     list_display = ["follower", "following", "created"]
     raw_id_fields = ["follower", "following"]
     list_filter = ["created"]
-    
+
+class ProfileAdmin(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class ExtendedUserAdmin(UserAdmin):
+    inlines = (ProfileAdmin,)
+
+
