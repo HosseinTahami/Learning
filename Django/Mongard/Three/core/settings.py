875302857f3cb69f06618ac17e4c3f3c50a0be7e
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-31 07:53:17 +0330

    Chore: Django Crispy Forms Configuration

--- Django/Mongard/Three/core/settings.py
+++ Django/Mongard/Three/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # Local
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django Crispy Forms Configuration
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
