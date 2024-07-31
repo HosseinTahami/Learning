70c8d3c059383738471840d7deb1e1a0e441f3f0
Hossein Tahami <hosseintahami2000@gmail.com>
2024-07-31 07:59:39 +0330

    Feat: Add Media Files Configurations

--- Django/Mongard/Three/core/settings.py
+++ Django/Mongard/Three/core/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 
+# Media Files
+# https://docs.djangoproject.com/en/5.0/topics/files/#managing-files/
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
