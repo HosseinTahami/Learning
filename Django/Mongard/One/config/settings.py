45c965f5b66b10d3aa57bac6ff474bfc3f93423e
Hossein Tahami <hosseintahami2000@gmail.com>
2025-07-11 21:05:54 +0330

    Add apps

--- Django/Mongard/One/config/settings.py
+++ Django/Mongard/One/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
+    'todos'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
