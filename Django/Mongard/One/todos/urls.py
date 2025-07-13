03f0020c70803d44100e8f8c725ed6fdf4220874
Hossein Tahami <hosseintahami2000@gmail.com>
2025-07-13 23:08:29 +0330

    Add urls

--- Django/Mongard/One/todos/urls.py
+++ Django/Mongard/One/todos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todos_list)
+    path('', views.todos_list),
+    path('detail/<int:todo_id>/', views.todo_detail, name='todo_detail'),
 ]
