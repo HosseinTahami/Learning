cc029fae9d0c70a9fccf35b6b63c503d509df378
Hossein Tahami <hosseintahami2000@gmail.com>
2025-07-13 23:08:17 +0330

    Add todo_detail function

--- Django/Mongard/One/todos/views.py
+++ Django/Mongard/One/todos/views.py
@@ -4,3 +4,7 @@ from . import models
 def todos_list(request):
     todos = models.Todo.objects.all()
     return render(request, 'todos.html', {"todos":todos})
+
+def todo_detail(request, todo_id):
+    todo = models.Todo.objects.get(id=todo_id)
+    return render(request, 'detail.html', {"todo":todo})
