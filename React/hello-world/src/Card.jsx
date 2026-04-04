5269403f69bc2e26c7e5fd88bd4b7b99529db3f8
Hossein Tahami <hosseintahami2000@gmail.com>
2026-04-04 13:02:45 +0200

    Add: Learning React Components

---
+++ React/hello-world/src/Card.jsx
@@ -0,0 +1,11 @@
+export const Card = () => {
+    return (
+        <div id="card">
+            <h2>Welcome</h2>
+            <p>
+                This is <span id="highlight">Paragraph</span> with text
+            </p>
+            <button>Click Me!</button>
+        </div>
+    );
+};
