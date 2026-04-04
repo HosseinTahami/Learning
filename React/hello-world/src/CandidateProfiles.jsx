f29f86528b7cdd67382400f66b504f4ab8b6a054
Hossein Tahami <hosseintahami2000@gmail.com>
2026-04-04 13:20:41 +0200

    Add: Learning React CandidateProfile Component

---
+++ React/hello-world/src/CandidateProfiles.jsx
@@ -0,0 +1,14 @@
+export const CandidateProfile = () => {
+
+
+    const name = "Peter Parker";
+    const role = "Web Developer";
+    const yearsOfExperience = 5;
+
+    return (
+        <>
+            <h2>{name}</h2>
+            <p>{role} for {yearsOfExperience} fucking years !</p>
+        </>
+    )
+}
