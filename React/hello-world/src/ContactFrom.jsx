851ede6cb96ad6666c80e10cbd9a77fa5a89ec7e
Hossein Tahami <hosseintahami2000@gmail.com>
2026-04-04 13:03:30 +0200

    Add: Learning React Components

---
+++ React/hello-world/src/ContactFrom.jsx
@@ -0,0 +1,9 @@
+export const ContactForm = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Your Fucking Name" />
+            <br />
+            <input type="email" placeholder="YourFucking Email" />
+        </form>
+    );
+}
