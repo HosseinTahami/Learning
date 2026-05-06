dd4fff9c20f0c68a548c6987a6d8709a5f09af1c
Hossein Tahami <hosseintahami2000@gmail.com>
2026-05-06 20:51:41 +0200

    Finish: Video 21

--- React/CodeVolution/state-demo/src/SimpleCounter.jsx
+++ React/CodeVolution/state-demo/src/SimpleCounter.jsx
@@ -3,11 +3,19 @@ import { useState } from "react";
 export const SimpleCounter = () => {
 
     const [count, setCount] = useState(0);
+    console.log("Render phase: Component rendering with count =", count);
+
 
     const CountFunc = () => {
-        console.log("Count: ", count);
+        console.log("Before setCount, count is:  ", count);
         setCount(count+1);
-        console.log("Count: ", count);
+        console.log("Still in trigger phase, After setCount + 1, count is: ", count);
+        setCount(count+19);
+        console.log("Still in trigger phase, After setCount + 19, count is: ", count);
+        setCount(count+10);
+        console.log("Still in trigger phase, After setCount + 10, count is: ", count);
+        setCount(count+12);
+        console.log("Still in trigger phase, After setCount + 12, count is: ", count);
     }
 
     return (
